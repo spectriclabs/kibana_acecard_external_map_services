7929371904cd1fc0f8621c4d15ab2f319b2eee15
Sean Sullivan <ssullivan@spectric.com>
2023-06-08 11:29:47 -0400

    Set config to point to a server that has layers with timestamps and respects CQL Polygon filters

--- common/config.ts
+++ common/config.ts
@@ -11,8 +11,8 @@ import { schema, TypeOf } from '@kbn/config-schema';
 export const mapConfigSchema = schema.object({
   urls: schema.arrayOf(schema.string(), {
     defaultValue: [
-      'https://mrdata.usgs.gov/services/active-mines', // Note these servers dont actually support the cql polygon filter
-      'https://mrdata.usgs.gov/services/phosphate', // Note these servers dont actually support the cql polygon filter
+      'https://labs.waterdata.usgs.gov/geoserver/ows', // GEOSERVER Layers huc08 and nhdarea have time filters and respect cql bounding boxes nhdarea times span from 1998-2005
+      // bulk of the features are 1999-06-17T04:00:00Z others are 2004-03-16T05:00:00Z and some have no fdate
     ],
   }),
   enabled: schema.boolean({ defaultValue: false }),
