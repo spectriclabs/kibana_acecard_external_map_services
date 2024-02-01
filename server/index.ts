a7cfddb8bd1e8d980b110bdfb61b0e0cbc95b12c
Sean Sullivan <ssullivan@spectric.com>
2024-02-01 13:28:30 -0500

    add version information into settings ui

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,7 @@ import {
 } from '@kbn/core/server';
 import { mapConfigSchema } from '../common/config';
 import type { AcecardEMSConfig } from '../common/config';
+import { schema } from '@kbn/config-schema';
 
 export const config: PluginConfigDescriptor<AcecardEMSConfig> = {
   exposeToBrowser: {
@@ -28,6 +29,28 @@ export class AcecardEMSPlugin implements Plugin<AcecardEMSPluginServerSetup> {
   }
 
   public setup(core: CoreSetup) {
+    const setVersion = (version:string) =>{
+      const name = "acecard:plugin"+ this.constructor.name;
+      const versionSettings:any = {}
+      versionSettings[name] = {
+        name,
+        description: `Commit id and message for ${this.constructor.name} version readonly do not change`,
+        category: ['acecard'],
+        order: 1,
+        type: 'string',
+        value: version,
+        readonly:false,
+        requiresPageReload: false,
+        schema: schema.string(),
+      }
+      core.uiSettings.register(versionSettings);
+    }
+    import("../common/version").then((version)=>{
+      setVersion(version.version)
+    }).catch(()=>{
+      setVersion("UNKNOWN")
+    })
+
     const mapConfig = this._initializerContext.config.get();
     return {
       config: mapConfig,
